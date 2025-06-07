b558cde83b4a75c07e977b573f8da95c90ec7fe7
John Janiczek <janiczek.john@gmail.com>
2025-06-07 09:21:39 -0600

    First Commit

---
+++ dsp_utils/dsp_utils.h
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.md file.
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:                 dsp_utils
+  vendor:             tbd
+  version:            1.0.0
+  name:               Transfer Function Editor
+  description:        A module for creating and editing transfer functions in audio applications.
+  website:            tbd
+  license:            proprietary/commercial
+  minimumCppStandard: 17
+
+  dependencies:       juce_core, juce_gui_basics, juce_data_structures, juce_audio_processors
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
+
+#pragma once
+#define DSP_UTILS_H_INCLUDED
+
+#include <juce_data_structures/juce_data_structures.h>
+
+#include "Source/CircularAudioBuffer.h"
