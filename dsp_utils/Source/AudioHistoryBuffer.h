01859eb33cf6bfb1947146dc753d75c797f68c93
John Janiczek <johnjaniczek@Johns-MacBook-Pro.local>
2025-09-26 14:25:38 -0600

    Add function for clearing buffer

--- dsp_utils/Source/AudioHistoryBuffer.h
+++ dsp_utils/Source/AudioHistoryBuffer.h
@@ -10,6 +10,8 @@ public:
     void pushSamples(const float* samples, int numSamples);
     void getHistory(float* outSamples, int numSamples) const;
     int getSize() const { return size; }
+    void clear() { std::fill(buffer.begin(), buffer.end(), 0.0f); writePos = 0; }
+
 private:
     std::vector<float> buffer;
     int writePos = 0;
