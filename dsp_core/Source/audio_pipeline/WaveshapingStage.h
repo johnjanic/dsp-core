adabca03696a8efe8efe840792b325a075bcb982
John Janiczek <johnjaniczek@Johns-MacBook-Pro.local>
2025-10-23 22:31:25 -0600

    Optimize code

--- dsp_core/Source/audio_pipeline/WaveshapingStage.h
+++ dsp_core/Source/audio_pipeline/WaveshapingStage.h
@@ -2,21 +2,19 @@
 
 #include "AudioProcessingStage.h"
 #include "../LayeredTransferFunction.h"
-#include <juce_core/juce_core.h>
 
 namespace dsp_core::audio_pipeline {
 
 /**
  * Applies waveshaping using LayeredTransferFunction.
- * Supports parallel processing via injected thread pool.
+ * Optimized for stereo processing (no threading overhead).
  */
 class WaveshapingStage : public AudioProcessingStage {
 public:
     /**
      * @param ltf Reference to transfer function model
-     * @param threadPool Thread pool for parallel processing (nullptr = serial)
      */
-    WaveshapingStage(dsp_core::LayeredTransferFunction& ltf, juce::ThreadPool* threadPool = nullptr);
+    explicit WaveshapingStage(dsp_core::LayeredTransferFunction& ltf);
 
     void prepareToPlay(double sampleRate, int samplesPerBlock) override;
     void process(juce::AudioBuffer<double>& buffer) override;
@@ -24,11 +22,7 @@ public:
     juce::String getName() const override { return "Waveshaping"; }
 
 private:
-    void processParallel(juce::AudioBuffer<double>& buffer);
-    void processSerial(juce::AudioBuffer<double>& buffer);
-
     dsp_core::LayeredTransferFunction& ltf_;
-    juce::ThreadPool* threadPool_;  // Not owned (injected dependency)
 };
 
 } // namespace dsp_core::audio_pipeline
